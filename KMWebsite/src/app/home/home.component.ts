c20baf6be058a490471dd99f7003f5861d90765d
markusmessneraau <115398958+markusmessneraau@users.noreply.github.com>
2025-01-27 08:28:10 +0100

    Mobile dvice

--- KMWebsite/src/app/home/home.component.ts
+++ KMWebsite/src/app/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  isMenuOpen = false; // Menü ist standardmäßig geschlossen
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen; // Zustand wechseln
+  }
 }
